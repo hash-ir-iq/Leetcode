8db9f506d324ee8d04befbb652e6bea6736005ee
Hashir Iqbal <hashiriqbal1453@gmail.com>
2026-07-16 15:41:38 +0500

    1610-XOR-OPERATION-IN-AN-ARRAY Stats: Time: 0 ms (100%), Space: 7.7 MB (80.82%) - LeetHub

--- 1610-xor-operation-in-an-array/1610-xor-operation-in-an-array.cpp
+++ 1610-xor-operation-in-an-array/1610-xor-operation-in-an-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int xorOperation(int n, int start) {
-        int ans=0,nums=0;
+        short ans=0;int nums=0;
         for(int i=0;i<n;i++){
              nums = start + 2 * i;
             ans = ans ^ nums;
